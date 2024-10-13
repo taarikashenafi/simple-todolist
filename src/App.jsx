680c089b91af4a8d31c564c2f05f69583eb6efff
Taarik Ashenafi <147209483+taarikashenafi@users.noreply.github.com>
2024-10-12 19:53:32 -0500

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import TodoInput from "./components/Todoinput"
 import TodoList from "./components/TodoList"
+import { VelocityScroll } from "./components/ui/scroll-based-velocity"
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -48,6 +49,11 @@ function App() {
 
   return (
     <>
+      <VelocityScroll
+      text="Todo List"
+      default_velocity={5}
+      className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+      />
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
       <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
